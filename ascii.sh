97b4d1db943576b1d57fbb7204f1a712950fcea8
Peyton Seagraves <peytonseagraves@gmail.com>
2024-10-23 15:33:49 +0000

    Added Ascii

---
+++ ascii.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+Green='\e[0;32m'
+NORMAL='\033[0m'
+
+echo -e "    ${Green} /___/\_$NORMAL"                               
+echo -e "   ${Green} _\   \/_/\__$NORMAL"                         
+echo -e " ${Green} __\       \/_/\ $NORMAL "                      
+echo -e " ${Green}  \   __    __ \ \ $NORMAL "                   
+echo -e " ${Green}  __\  \_\   \_\ \ \  __ $NORMAL"               
+echo -e " ${Green} /_/\\   __   __  \ \_/_/\ $NORMAL"          
+echo -e " ${Green} \_\/_\__\/\__\/\__\/_\_\/ $NORMAL"         
+echo -e " ${Green}   \_\/_/\       /_\_\/ $NORMAL "            
+echo -e " ${Green}      \_\/       \_\/ $NORMAL "    
